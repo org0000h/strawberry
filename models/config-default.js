22f07846c3e38c26354af00ba2be9cded39a06ae
org0000h <yazhou05@gmail.com>
2019-03-23 20:44:06 +0800

    add models config

---
+++ models/config-default.js
@@ -0,0 +1,10 @@
+var config = {
+    dialect: 'mysql',
+    database: 'db1',
+    username: 'root',
+    password: 'google',
+    host: 'url',
+    port: 3306
+};
+
+module.exports = config;
---
+++ models/config-override.js
@@ -0,0 +1,8 @@
+var config = {
+    database: 'production',
+    username: 'realusername',
+    password: 'secret-password',
+    host: '192.168.1.199'
+};
+
+module.exports = config;
